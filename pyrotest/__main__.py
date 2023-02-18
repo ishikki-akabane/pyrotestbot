65607e8ab19515afe5102d13969a9eb87daebbd1
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2023-02-18 15:19:39 +0530

    Create __main__.py

---
+++ pyrotest/__main__.py
@@ -0,0 +1,26 @@
+from pyrogram import Client
+from pytgcalls import idle
+import os
+from pyrotest import API_ID, API_HASH, BOT_TOKEN, DOWNLOAD_DIRECTORY
+import asyncio
+
+
+Client(
+    name="pyrotest",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    workers=min(32, os.cpu_count() + 4),
+    workdir=DOWNLOAD_DIRECTORY,
+    sleep_threshold=60,
+    in_memory=True,
+    plugins={"root": "pyrotest.plugins"},
+).start()
+
+
+loop = asyncio.get_event_loop_policy().get_event_loop()
+loop.run_until_complete(load_start())
+
+
+idle()
+loop.close()
